ca7f7e30d84d76989d69285f8c79fb4b9c905eec
YangYang <ss880309@gmail.com>
2021-03-15 22:31:31 +0800

    Adjust README and add ologn version

--- README.md
+++ README.md
@@ -3,6 +3,17 @@
 ## O(n) version
 If we iterative the number from 1 to n, we could easily count how many 7 within the range.
 
+## O(logn) version
+If we focus on every 7's sum of digit, we could find the regular.  
+In 1 to 10 case, 7 apears 1 time in unit digit.  
+In 1 to 100 case, 7 apears 10 time in tens digit.  
+In 1 to 1000 case, 7 apears 100 time in hundreds digit.  
+...
+
+So, if we want to count-7 of Nth digit, we could says:
+S(Nth) = (capitalDigit) * N + S((N-1)th) + ...
+
+
 ## How to test Sample test
 ```
 npm run test
--- index.js
+++ index.js
@@ -1,23 +1,64 @@
+// O(n) version
+// const count7 = (n) => {
+//     let count = 0;
+//     for(let i=1;i<=n;i++) {
+//         if(String(i).includes(`7`)) {
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+
+
+// O(logn) version
 const count7 = (n) => {
-    let count = 0;
-    for(let i=1;i<=n;i++) {
-        if(String(i).includes(`7`)) {
-            count++;
-        }
+    if(n < 7) {
+        return 0;
     }
-    return count;
-}
+    
+    // Number of N's digits
+    let numOfDigits = Math.floor(Math.log10(n));
+    
+    // The table includes count-7 of 10^i to 10^(i+1)-1.
+    // 1, 1~10, 10~100, ......
+    let table = [0, 1];
+    for (let i = 2; i <= numOfDigits; i++)  {
+        table[i] = table[i - 1] * 9 + Math.ceil(Math.pow(10, i - 1)); 
+    }
+
+    // capitalDigit = n / 10^(numOfDigits)
+    let capitalDigit = Math.floor(n / Math.ceil(Math.pow(10, numOfDigits))); 
+
+
+    if (capitalDigit === 7) {
+        // e.g. 789
+        // if capital digit is equal to 7, we could count of 1 to 699 and add 700 to 789.
+        return (capitalDigit) * table[numOfDigits] + (n % Math.ceil(Math.pow(10, numOfDigits))) + 1; 
+    } else if (capitalDigit > 7) {
+        // e.g. 897
+        // if capital digit is greater than 7, we could count of 1 to 699, count 700 to 799 and count 800 to 897.
+        return (capitalDigit - 1) * table[numOfDigits] + Math.ceil(Math.pow(10, numOfDigits)) + count7(n % Math.ceil(Math.pow(10, numOfDigits))); 
+    } else {
+        // e.g. 345
+        // if capital digit is lower than 7, we could count of 1 to 299 and count 300 to 345.
+        return (capitalDigit) * table[numOfDigits] + count7(n % Math.ceil(Math.pow(10, numOfDigits))); 
+    }
+};
 
 const print = (n) => {
     console.log(`g(${n}) = ${count7(n)}`);
 }
 
-// print(1);
-// print(7);
-// print(20);
-// print(70);
-// print(100);
-// print(1000);
+print(1);
+print(7);
+print(20);
+print(70);
+print(100);
+print(500)
+print(700);
+print(789);
+print(800);
+print(1000);
 
 module.exports = {
     count7
